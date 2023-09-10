5adfa3a33c407e061c2300f9f760dde425650fde
Nadeeja Kalhara <elkalhara@outlook.com>
2023-09-11 03:43:14 +0530

    ac

--- js/myjs.js
+++ js/myjs.js
@@ -57,10 +57,7 @@ const Toast = Swal.mixin({
    toast: true,
    position: 'bottom-end',
    showConfirmButton: false,
-   didOpen: (toast) => {
-     toast.addEventListener('mouseenter', Swal.stopTimer)
-     toast.addEventListener('mouseleave', Swal.resumeTimer)
-   }
+
  })
  
  Toast.fire({
